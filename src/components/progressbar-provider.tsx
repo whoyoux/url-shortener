3efa0fea60530c7b2333c3a795c39a5dee920f8a
whoyoux <48690153+whoyoux@users.noreply.github.com>
2023-11-15 00:59:41 +0100

    add changing route progress bar

---
+++ src/components/progressbar-provider.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+import { Next13ProgressBar } from "next13-progressbar";
+
+const ProgressBarProvider = () => {
+    return <Next13ProgressBar color="#16A34A" />;
+};
+
+export default ProgressBarProvider;
