cc50d632df46b13c520b8367069eafc110207b5b
whoyoux <48690153+whoyoux@users.noreply.github.com>
2023-11-20 17:23:37 +0100

    Update page.tsx

--- src/app/r/[slug]/page.tsx
+++ src/app/r/[slug]/page.tsx
@@ -6,38 +6,47 @@ import React from "react";
 const RedirectPage = async ({ params }: { params: { slug: string } }) => {
     const shortUrl = params.slug;
 
-    const data = await prisma.url.findUnique({
-        where: {
-            shortUrl,
-        },
-        select: {
-            originalUrl: true,
-            id: true,
-        },
-    });
-
-    if (!data) {
-        return (
-            <main className="pt-10 text-center">
-                <H1>No shorted link found!</H1>
-            </main>
-        );
-    }
-
-    await prisma.url.update({
-        where: {
-            id: data?.id,
-        },
-        data: {
-            totalClicks: {
-                increment: 1,
+    try {
+        const data = await prisma.url.findUnique({
+            where: {
+                shortUrl,
             },
-        },
-    });
+            select: {
+                originalUrl: true,
+                id: true,
+            },
+        });
 
-    console.log("Redirecting to: ", data.originalUrl, " Time: ", new Date());
+        if (!data) {
+            return (
+                <main className="pt-10 text-center">
+                    <H1>No shorted link found!</H1>
+                </main>
+            );
+        }
 
-    redirect(data.originalUrl);
+        await prisma.url.update({
+            where: {
+                id: data?.id,
+            },
+            data: {
+                totalClicks: {
+                    increment: 1,
+                },
+            },
+        });
+
+        console.log(
+            "Redirecting to: ",
+            data.originalUrl,
+            " Time: ",
+            new Date()
+        );
+
+        redirect(data.originalUrl);
+    } catch (err) {
+        return <H1>Something went wrong!</H1>;
+    }
 };
 
 export default RedirectPage;
