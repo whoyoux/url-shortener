46293381608c854bf38e3fe881ced7821a2069ab
whoyoux <48690153+whoyoux@users.noreply.github.com>
2023-12-03 16:07:12 +0100

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,23 +5,28 @@ import { UrlResponseData } from "./app/api/url/route";
 export async function middleware(request: NextRequest) {
     console.log(request.nextUrl.pathname.split("/")[2]);
 
-    const res = await fetch("http://localhost:3000/api/url", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-            shortUrl: request.nextUrl.pathname.split("/")[2],
-        }),
-    });
+    try {
+        const res = await fetch("http://localhost:3000/api/url", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                shortUrl: request.nextUrl.pathname.split("/")[2],
+            }),
+        });
 
-    const data = (await res.json()) as UrlResponseData;
-    console.log(data);
+        const data = (await res.json()) as UrlResponseData;
+        console.log(data);
 
-    if (data.status === "success")
-        return NextResponse.redirect(new URL(`${data.url}`, request.url));
+        if (data.status === "success")
+            return NextResponse.redirect(new URL(`${data.url}`, request.url));
 
-    return NextResponse.redirect(new URL("/404", request.url));
+        return NextResponse.redirect(new URL("/404", request.url));
+    } catch (err) {
+        console.error(err);
+        return NextResponse.redirect(new URL("/404", request.url));
+    }
 }
 
 export const config = {
