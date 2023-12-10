7364df0cf1470501db2d1fb6cd3540aacc9030e8
whoyoux <48690153+whoyoux@users.noreply.github.com>
2023-12-10 17:22:14 +0100

    add some security stuff

--- src/app/account/page.tsx
+++ src/app/account/page.tsx
@@ -3,24 +3,22 @@ import { H2 } from "@/components/ui/H2";
 import { Button } from "@/components/ui/button";
 import AuthGuard from "@/lib/AuthGuard";
 import React from "react";
-import { cancelSubscription } from "../(actions)/subscription";
+import { cancelSubscription } from "../../actions/subscription";
 
 const AccountPage = async () => {
-    const session = await AuthGuard();
+  const session = await AuthGuard();
 
-    return (
-        <div>
-            <H1>My account</H1>
-            <form className="mt-5" action={cancelSubscription}>
-                <H2>My current membership plan: {session.user.plan}</H2>
-                {session.user.plan === "PRO" && (
-                    <Button variant="destructive">
-                        Cancel my subscription
-                    </Button>
-                )}
-            </form>
-        </div>
-    );
+  return (
+    <div>
+      <H1>My account</H1>
+      <form className="mt-5" action={cancelSubscription}>
+        <H2>My current membership plan: {session.user.plan}</H2>
+        {session.user.plan === "PRO" && (
+          <Button variant="destructive">Cancel my subscription</Button>
+        )}
+      </form>
+    </div>
+  );
 };
 
 export default AccountPage;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,64 +6,63 @@ import { P } from "@/components/ui/P";
 import { H1 } from "@/components/ui/H1";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { createShortUrl } from "@/app/(actions)/createShortUrl";
+import { createShortUrl } from "@/actions/createShortUrl";
 import { useSession } from "next-auth/react";
 import { H3 } from "@/components/ui/H3";
 import Link from "next/link";
 
 const initialState = {
-    message: "",
+  message: "",
 };
 
 const SubmitButton = () => {
-    const { data: session, status } = useSession();
-    const { pending } = useFormStatus();
-    return (
-        <Button
-            className="w-full"
-            aria-disabled={pending || !session}
-            disabled={pending || !session}
-        >
-            {session
-                ? "Shorten!"
-                : status === "loading"
-                ? "Loading..."
-                : "You have to login"}
-        </Button>
-    );
+  const { data: session, status } = useSession();
+  const { pending } = useFormStatus();
+  return (
+    <Button
+      className="w-full"
+      aria-disabled={pending || !session}
+      disabled={pending || !session}
+    >
+      {session
+        ? "Shorten!"
+        : status === "loading"
+        ? "Loading..."
+        : "You have to login"}
+    </Button>
+  );
 };
 
 export default function Home() {
-    const [state, formAction] = useFormState(createShortUrl, initialState);
+  const [state, formAction] = useFormState(createShortUrl, initialState);
 
-    return (
-        <main>
-            <form
-                className="flex flex-col items-center mt-10 md:mt-20 gap-6 max-w-xl mx-auto"
-                action={formAction}
-            >
-                <H1>Shorten your URL</H1>
-                <Input
-                    type="url"
-                    placeholder="Enter a long URL"
-                    className="mt-8"
-                    name="originalUrl"
-                    required
-                />
-                <SubmitButton />
-                <P>
-                    You will be redirected after successfully creation of
-                    shortened url
-                </P>
+  return (
+    <main>
+      <form
+        className="flex flex-col items-center mt-10 md:mt-20 gap-6 max-w-xl mx-auto"
+        action={formAction}
+      >
+        <H1>Shorten your URL</H1>
+        <Input
+          type="url"
+          placeholder="Enter a long URL"
+          className="mt-8"
+          name="originalUrl"
+          required
+        />
+        <SubmitButton />
+        <P>
+          You will be redirected after successfully creation of shortened url
+        </P>
 
-                {/* <div>
+        {/* <div>
           <H3>
             <Link href={`r/${state?.message}`}>
               {`localhost:300/r/${state?.message}`}
             </Link>
           </H3>
         </div> */}
-            </form>
-        </main>
-    );
+      </form>
+    </main>
+  );
 }
